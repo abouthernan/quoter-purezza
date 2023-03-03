fbd3c8473314a67250c557fcb0085e0bdd091a08
Cristhian Melo <rxtsel@hotmail.com>
2023-03-03 11:51:09 -0500

    chore: update costos text

--- src/components/progressBar/index.tsx
+++ src/components/progressBar/index.tsx
@@ -8,7 +8,7 @@ interface BarProps {
 export function ProgressBar ({ value, black = false }: BarProps): JSX.Element {
   return (
     <>
-      <span className={`${styles.span} ${styles.top}`}>{100 - value}%</span>
+      <span className={`${styles.span} ${styles.top} ${black ? styles.black : ''}`}>{100 - value}%</span>
       <progress className={`${styles.bar} ${black ? styles.black : ''}`} value={value} max="100" />
       <span className={`${styles.span} ${black ? styles.black : ''}`}>{value}%</span>
     </>
