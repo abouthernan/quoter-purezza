6664b7e16e6fbb94c938cefcddec202f1dbd0edc
Cristhian Melo <yourmail@hotmail.com>
2023-03-14 20:14:07 -0500

    chore: add span with text

--- src/components/progressBar/index.tsx
+++ src/components/progressBar/index.tsx
@@ -9,8 +9,10 @@ export function ProgressBar ({ value, black = false }: BarProps): JSX.Element {
   return (
     <>
       <span className={`${styles.span} ${styles.top} ${black ? styles.black : ''}`}>{100 - value}%</span>
+      <span className={styles.rentabilidad}>rentabilidad</span>
       <progress className={`${styles.bar} ${black ? styles.black : ''}`} value={value} max="100" />
       <span className={`${styles.span} ${black ? styles.black : ''}`}>{value}%</span>
+      <span className={styles.costos}>costos</span>
     </>
   )
 }
